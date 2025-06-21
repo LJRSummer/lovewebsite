4fe175466111aaeabc6ef25e03aaddadb1f046b7
LJRSummer <1075340816@qq.com>
2025-06-22 02:08:33 +0800

    feat: 增加route.ts

---
+++ src/app/api/images/route.ts
@@ -0,0 +1,15 @@
+import { NextResponse } from "next/server";
+import fs from "fs";
+import path from "path";
+
+export async function GET() {
+  const albumsDir = path.join(process.cwd(), "public/albums");
+  const files = fs.readdirSync(albumsDir);
+  const images = files
+    .filter((file) => /\.(jpg|jpeg|png|gif)$/i.test(file))
+    .map((file) => ({
+      url: "/albums/" + file,
+      title: file.replace(/\.[^/.]+$/, ""),
+    }));
+  return NextResponse.json(images);
+}
